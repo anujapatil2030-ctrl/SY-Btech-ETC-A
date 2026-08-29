2660d77318fbd8759acc498bf8b7d121ad18a90a
anujapatil2030-ctrl <anujapatil2030@gmail.com>
2026-08-29 22:26:21 +0530

    Create extModule.py

---
+++ extModule.py
@@ -0,0 +1,8 @@
+#Problem 2:downloa a exr=ternal module
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("I will speak this text")
+engine.runAndWait()
+"""
+   this module basically lets the engine to speak the text that we have written aloud
+"""
