47526c0199e24ff9da77258e2fa727ef085155a3
anujapatil2030-ctrl <anujapatil2030@gmail.com>
2026-09-03 18:42:49 +0530

    Create squareOf_num.py

---
+++ squareOf_num.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the number:"))
+print("Square of the number is ",a*a)
+#-------------------------------------------
+#           OR
+#-------------------------------------------
+print("Square of the number is ",a**2)   # through this we can also find other powers of the number
+
+# no need to use '\n' in every print 
